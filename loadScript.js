f2a29cc3d3f84a06f84ddca6f6ba86983b2cff24
Jiahan Wang <jiahanwang1990@gmail.com>
2014-09-19 16:52:00 -0700

    Added: loadScript

---
+++ loadScript.js
@@ -0,0 +1,28 @@
+/**
+*
+* Catergory: Function
+* Usage:  Dymanically load external javascript file
+* Source: High Performance JavaScript - Nicholas C.Zakas
+*
+**/
+
+function loadScript (url, callback) {
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	if(script.readyState){
+		script.onreadystatechange = function () {
+			if(script.readyState == "loaded" || script.readyState == "complete"){
+				script.readyState = null;
+				callback();	
+			}
+		}
+	}
+	else{
+		script.onload = function () {
+			callback();
+		}	
+	}
+	script.src = url;
+	document.getElementsByTagName("head")[0].appendChind(script);
+}
+
