9f2886b2596f76c46514ea6a8320d6a47912eeaa
Jiahan Wang <jiahanwang1990@gmail.com>
2014-09-28 23:34:30 -0700

    Added: load style sheet

--- performance/loadScript.js
+++ performance/loadScript.js
@@ -1,7 +1,7 @@
 /**
 *
 * Catergory: Peformance
-* Usage:  Dymanically load external javascript file
+* Usage:  Dymanically load javascript 
 * Source: High Performance JavaScript - Nicholas C.Zakas
 *
 **/
@@ -9,6 +9,7 @@
 function loadScript (url, callback) {
 	var script = document.createElement("script");
 	script.type = "text/javascript";
+	//for IE
 	if(script.readyState){
 		script.onreadystatechange = function () {
 			if(script.readyState == "loaded" || script.readyState == "complete"){
@@ -17,12 +18,26 @@ function loadScript (url, callback) {
 			}
 		}
 	}
+	// for normal
 	else{
 		script.onload = function () {
 			callback();
 		}	
 	}
 	script.src = url;
-	document.getElementsByTagName("head")[0].appendChind(script);
+	document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+function loadStringScript (code){
+   var script = document.createElement('script');
+   script.type = 'text/javascript';
+   try{
+      // for normal
+      script.appendChild(document.createTextNode(code));
+   }catch(e){
+      // for IE
+      script.text = code;
+   }
+   document.body.appendChild(script);
 }
 
---
+++ performance/loadStyle.js
@@ -0,0 +1,43 @@
+/**
+*
+* Catergory: Peformance
+* Usage:  Dymanically load style sheet
+* Source: High Performance JavaScript - Nicholas C.Zakas
+*
+**/
+
+function loadStyle (url, callback){
+   var link = document.createElement('link');
+   link.ref = 'stylesheet';
+   link.type = 'text/css';
+   // for IE
+   if(link.readyState){
+      if(link.readyState == 'loaded' || link.readyState == "complete"){
+      link.readyState = null;
+      callback();
+      }
+   
+   }else{
+      // for normal broswers
+      link.onload = function () {
+         callback();
+      }
+   }
+   link.href = url;
+   document.getElementsByTagName('head')[0].appendChild(link);
+
+}
+
+function loadStringStyle (code) {
+   var style = document.createElement('style');
+   style.type = 'text/css';
+   try{
+      // for normal broswers
+      style.appendChild(document.createTextNode(code));
+   }catch(e){
+      // for IE
+      style.styleSheet.cssText = code;
+   }
+   document.getElementsByTagName('head')[0].appendChild(style);
+}
+
