c0a6c18ca83ccd10ecf2f86cc954af8d1be8d415
Jiahan Wang <jiahanwang1990@gmail.com>
2014-10-01 23:28:32 -0700

    Added: cross browser event handler

---
+++ dom/EventUtil.js
@@ -0,0 +1,55 @@
+/**
+*
+* Catergory: DOM 
+* Usage:  Cross-browser Event handler
+* Source: Pro JavaScript - Nicholas C.Zakas
+*
+**/
+var EventUtil = {
+
+   addHandler : function (element, type, handler) {
+      if(element.addEventListener){
+         element.addEventListner(type, handler, false);
+      }else if(element.attachEvent){
+         element.attachEvent('on' + type, handler);
+      }else{
+         element['on' + type] = handler;
+      }
+   },
+
+   removeHandler : function (element, type, handler){
+      if(element.removeEventListener){
+         element.removeEventListener(type, handler, false);
+      }else if(element.detachEvent){
+         element.detachEvent('on' + type, handler);
+      }else {
+          element['on' + type] = null;
+      }
+   },
+
+   getEvent : function (event) {
+      return event ? event : window.event;
+   }, 
+
+   getTarget : function (event) {
+      return event.target ? event.target : event.srcElement;
+   },
+
+   preventDefault : function (event) {
+      if(event.preventDefault){
+         event.preventDefault();
+      }else{
+         event.returnValue = false;
+      }
+   },
+   
+   stopPropagation : function (event) {
+      if(event.stopPropagation){
+         event.stopPropagation;
+      }else{
+         event.cancelBubble = true;
+      }
+   }
+
+}
+
