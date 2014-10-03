400f690d28909ff8b2cce8021c888801a580379a
Jiahan Wang <jiahanwang1990@gmail.com>
2014-10-02 23:17:13 -0700

    Modified: event handler

--- dom/EventUtil.js
+++ dom/EventUtil.js
@@ -6,7 +6,7 @@
 *
 **/
 var EventUtil = {
-
+   
    addHandler : function (element, type, handler) {
       if(element.addEventListener){
          element.addEventListner(type, handler, false);
@@ -26,7 +26,8 @@ var EventUtil = {
           element['on' + type] = null;
       }
    },
-
+   
+   // for all
    getEvent : function (event) {
       return event ? event : window.event;
    }, 
@@ -39,6 +40,7 @@ var EventUtil = {
       if(event.preventDefault){
          event.preventDefault();
       }else{
+         // for IE
          event.returnValue = false;
       }
    },
@@ -47,9 +49,85 @@ var EventUtil = {
       if(event.stopPropagation){
          event.stopPropagation;
       }else{
+         // for IE
          event.cancelBubble = true;
       }
-   }
+   },
+   
+   // for mouse events
+   getPageX : function (event) {
+      return event.pageX ?
+             event.pageX :
+             event.clientX + (document.body.scrollLeft || document.documentElement.scrollLeft);
+   },
+   
+   // for mouse events
+   getPageY : function (event) {
+      return event.pageY ?
+             event.pageY :
+             event.clientY + (document.body.scrollTop || document.documentElement.scrollTop);
+   },
+
+   // for mouseleave, mouseenter, mouseover, mouseout
+   getRelatedTarget : function (event) {
+      if (event.relatedTarget) {
+         return event.relatedTarget;
+      // for IE
+      } else if(event.toElement) {
+         return event.toElement;
+      } else if(event.fromElement) {
+         return event.fromElement; 
+      } else {
+         return null;
+      }
+   },
+   
+   // for mouse events
+   getButton : function (event) {
+      if (document.implementation.hasFeature('MouseEvents', '2.0')) {
+         return event.button;
+      } else {
+         // for IE
+         switch (event.button) {
+            case 0: 
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+               return 0;
+            case 2:
+            case 6:
+               return 2;
+            case 4:
+               return 1;
+         }
+      }
+   },
+
+   // for mousewheel
+   getWheelDelta : function (event) {
+      if (event.wheelDelta) {
+         return (client.engine.opera && client.engine.opera < 9.5 ? 
+                -event.wheelDelta : event.wheelDelta);
+      } else {
+         return -event.detail * 40;
+      }
+   },
+
+   // for keypress only, not keyup or keydown
+   getChar : function (event) {
+      var char;
+      if (event.which == null) { 
+         char= String.fromCharCode(event.keyCode);  // old IE 
+      }
+      else if (event.which != 0 && event.charCode != 0) {  
+         char= String.fromCharCode(event.which); // All others else 
+      }
+      else {
+         char = null; // special key
+      }
+      return char;
+   },
 
 }
 
